ed84ca3eb013e8e5925cbd6616cf0a54dac4452f
OpTelecHD <henke.nachtegaal@gmail.com>
2018-11-04 15:58:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const fs = require ("fs");
 const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
+
+const client = new commando.Client({
+    commandPrefix: '<Insert Your Prefix Here>',
+    owner: '<Insert Your User ID Here>',
+    disableEveryone: true,
+    unknownCommandResponse: false
+});
+
 client.registry.registerGroup('simple', 'Simple');
 client.registry.registerDefaults();
 client.registry.registerCommandsIn (__dirname + '/commands');
