b0ff3b5aa6fadfe685add18c1f94016ed3291276
OpTelecHD <henke.nachtegaal@gmail.com>
2018-11-14 22:57:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TOKEN = process.env.TOKEN;
 const client = new commando.Client({
     unknownCommandResponse: false
 });
-bot.remove_command('help');
+
 client.registry.registerGroup('simple', 'Simple');
 client.registry.registerDefaults();
 client.registry.registerCommandsIn (__dirname + '/commands');
